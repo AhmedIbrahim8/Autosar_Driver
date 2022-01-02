6c068d1021638920609e0f9874520493858ed566
Ahmed-Ibrahim <eng.ahmedibrahim21@gmail.com>
2022-01-02 03:39:52 +0200

    Port Ids

--- Port Driver Autosar Driver/Port.h
+++ Port Driver Autosar Driver/Port.h
@@ -8,3 +8,32 @@
 *
 *Author       : Ahmed Ibrahim
 *******************************************************/
+
+#ifndef PORT_H
+#define PORT_H
+
+/* ID of the Company in the Autosar */
+#define PORT_VENDOR_ID            (1000U)
+
+/* Port Module ID*/
+#define PORT_MODULE_ID            (124U)
+
+/* Port Instance ID */
+#define PORT_INSTANCE_ID          (0U)
+
+/* SoftWare Module Version */
+#define PORT_SW_MAJOR_VERSION     (1U)
+#define PORT_SW_MINOR_VERSION     (0U)
+#define PORT_SW_PATCH_VERSION     (0U)
+
+/*
+ * AUTOSAR Version 4.0.3
+ */
+#define DIO_AR_RELEASE_MAJOR_VERSION   (4U)
+#define DIO_AR_RELEASE_MINOR_VERSION   (0U)
+#define DIO_AR_RELEASE_PATCH_VERSION   (3U)
+
+/* Defines for Port status */
+#define PORT_INITIALIZED               (1U)
+#define PORT_NOT_INITIALIZED               (0U)
+#endif
