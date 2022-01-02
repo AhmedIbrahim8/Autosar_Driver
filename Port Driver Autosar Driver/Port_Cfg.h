c6fb04e5b469181c387c960549816c83a88b9f7d
Ahmed-Ibrahim <eng.ahmedibrahim21@gmail.com>
2022-01-02 05:10:55 +0200

    guarding of configuration file

---
+++ Port Driver Autosar Driver/Port_Cfg.h
@@ -0,0 +1,23 @@
+ /******************************************************************************
+ *
+ * Module      : Port
+ *
+ * File Name   : Port_Cfg.h
+ *
+ * Description : Pre-Compile Configuration Header file for TM4C123GH6PM Microcontroller - Port Driver
+ *
+ * Author      : Ahmed Ibrahim
+ ******************************************************************************/
+
+#ifndef PORT_CFG_H
+#define PORT_CFG_H
+
+
+
+
+
+
+
+
+
+#endif
