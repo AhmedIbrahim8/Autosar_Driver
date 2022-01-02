28967da07b2f024189c68d272a0ec03418026d6c
Ahmed-Ibrahim <eng.ahmedibrahim21@gmail.com>
2022-01-02 05:22:24 +0200

    PortGeneral Container is added

--- Port Driver Autosar Driver/Port_Cfg.h
+++ Port Driver Autosar Driver/Port_Cfg.h
@@ -12,12 +12,29 @@
 #ifndef PORT_CFG_H
 #define PORT_CFG_H
 
+/* SoftWare Module Version */
+#define PORT_SW_MAJOR_VERSION     (1U)
+#define PORT_SW_MINOR_VERSION     (0U)
+#define PORT_SW_PATCH_VERSION     (0U)
 
+/*
+ * AUTOSAR Version 4.0.3
+ */
+#define PORT_AR_RELEASE_MAJOR_VERSION   (4U)
+#define PORT_AR_RELEASE_MINOR_VERSION   (0U)
+#define PORT_AR_RELEASE_PATCH_VERSION   (3U)
 
+/* Pre-compile option for Development Error Detection         */
+#define PORT_DEV_ERROR_DETECT         (STD_ON)
 
+/* Pre-compile option to presence of Port_SetPinDirection Api */
+#define PORT_SET_PIN_DIRECTION_API    (STD_ON)
 
+/* Pre-compile option to presence of Port_SetPinMode Api      */
+#define PORT_SET_PIN_MODE_API         (STD_ON)
 
-
+/* Pre-compile option to presence of Port_VerionInfo Api      */
+#define PORT_VERSION_INFO_API         (STD_ON)
 
 
 #endif
