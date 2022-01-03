3aa3e473f814ae0bd102c638c1aa247cb16827d5
Ahmed-Ibrahim <eng.ahmedibrahim21@gmail.com>
2022-01-03 19:38:10 +0200

    Port.c is added

---
+++ Port Driver Autosar Driver/Port.c
@@ -0,0 +1,13 @@
+ /******************************************************************************
+ *
+ * Module: Port
+ *
+ * File Name: Port.c
+ *
+ * Description: Source file for TM4C123GH6PM Microcontroller - Port Driver
+ *
+ * Author: Ahmed Ibrahim
+ ******************************************************************************/
+
+#include "Port.h"
+#include "Port_Regs"
